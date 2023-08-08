1abe0f537fc0c28655f6354275e705ca8b8a82d3
Piravit <piravit.chenpittaya@gmail.com>
2023-08-08 19:53:41 +0700

    update Docker labels & image name

--- startserver.sh
+++ startserver.sh
@@ -4,4 +4,4 @@ docker run -d -i -t --name nmrih-server \
 	-p 27015:27015/udp \
 	-v ./nmrih:/home/steam/nmrih/ \
 	--restart=unless-stopped \
-	ghcr.io/karnzx/nmrih-server
+	ghcr.io/karnzx/nmrih-docker:latest
